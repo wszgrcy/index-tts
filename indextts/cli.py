2f03b1ae96fd722f12c1241fa5fdc9ed65642564
chen <wszgrcy@gmail.com>
2025-09-08 23:27:29 +0800

    1

--- indextts/cli.py
+++ indextts/cli.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import warnings
+from indextts.infer_v2 import IndexTTS2
 # Suppress warnings from tensorflow and other libraries
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
